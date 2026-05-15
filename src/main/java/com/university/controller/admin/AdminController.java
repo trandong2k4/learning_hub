d3dea3639b8b02f3792632a1058fc30256a6e9b2
trandong2k4 <nguyendong20041@gmail.com>
2026-05-15 17:18:20 +0700

    update

--- src/main/java/com/university/controller/admin/AdminController.java
+++ src/main/java/com/university/controller/admin/AdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin/test")
+@RequestMapping("/api/auth/test")
 @RequirePermission("ADMIN_DASHBOARD_ADMIN_VIEW")
 public class AdminController {
 
